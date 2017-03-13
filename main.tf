228ae587cb70212157ba0146784af029680f7eee
prissy12 <lastone2cu@yahoo.com>
2017-03-12 21:08:08 -0400

    mains3
    code for S3

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resources "aws_s3_bucket" "terraform_state" {
-bucket = "terraform-up-and-running-state"
+bucket = "toms1-terraform-up-and-running-stateterraform-up-and-running-state"
 
 versioning {
 enabled = true
@@ -12,4 +12,23 @@ enabled = true
 lifecycle {
 prevent_destory = true
 }
+}
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.terraform_state.arn}"
+  
+ # Configure Terragrunt to use DynamoDB for locking
+ lock = {
+ backend = "dynamodb"
+ config {    state_file_id = "global/s3"
+ }}
+ 
+ # Configure Terragrunt to automatically store tfstate files in S3
+ remote_state = {
+ backend = "s3"
+ config {
+ encrypt = "true"
+ bucket  = "(toms1-terraform-up-and-running-stateterraform-up-and-running-state)"
+ key     = "global/s3/terraform.tfstate"
+ region  = "us-east-1"
+ }
 }
